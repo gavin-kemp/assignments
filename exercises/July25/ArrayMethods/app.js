e66c4b5e5e8562db3fc01c24b90c85da6bd98092
gavin-kemp <13gkemp@gmail.com>
2017-07-25 13:42:28 -0600

    Finished arrayMethod Exercise

--- exercises/July25/ArrayMethods/app.js
+++ exercises/July25/ArrayMethods/app.js
@@ -6,9 +6,38 @@ vegetables.pop();
 
 console.log(vegetables);
 
-var indexOfOrange = fruit.shift();
+fruit.shift();
 
 console.log(fruit);
 
+var indexOfOrange = fruit.indexOf("orange");
+
+console.log("Index of orange is: " + fruit.indexOf("orange"));
+
 fruit.push(indexOfOrange);
 
+console.log(fruit);
+
+var vegArrLength = vegetables.length;
+
+console.log("Vegetable length: " + vegetables.length);
+
+vegetables.push(vegArrLength);
+
+console.log(vegetables);
+
+var food = fruit.concat(vegetables);
+
+console.log(food);
+
+food.splice(4, 2);
+
+console.log("Spliced Array: " + food);
+
+console.log("Reversed Array: " + food.reverse());
+
+var reversedArray = food.reverse();
+
+var arrayToString = reversedArray.join(", ");
+
+console.log("Array as string: " + arrayToString);
