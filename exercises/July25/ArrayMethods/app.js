416293804a4688af977b203b10f1110d381ac8f9
gavin-kemp <13gkemp@gmail.com>
2017-07-25 13:37:20 -0600

    Removed Dsstore, started arrayMethod exercise

---
+++ exercises/July25/ArrayMethods/app.js
@@ -0,0 +1,14 @@
+var fruit = ["banana", "apple", "orange", "watermelon"];  
+var vegetables = ["carrot", "tomato", "pepper", "lettuce"];
+
+
+vegetables.pop();
+
+console.log(vegetables);
+
+var indexOfOrange = fruit.shift();
+
+console.log(fruit);
+
+fruit.push(indexOfOrange);
+
