eb4cc374937db168eb519dd6d714e49df1b651ca
gavin-kemp <13gkemp@gmail.com>
2017-08-01 09:12:34 -0600

    Finished morning warmup and part of Mario Pest Control

---
+++ warmups/August1/Last Digit Same/app.js
@@ -0,0 +1,19 @@
+// Given two non-negative int values, return true if they have the same last digit,
+// such as with 27 and 57. Note that the % 'mod' operator computes remainders,
+// so 17 % 10 is 7.
+
+function lastDigitSame(a, b){
+  
+  if((a % 10) === (b % 10)){
+    
+    return true;
+    
+  }
+  else {
+    
+    return false;
+  }
+  
+}
+
+lastDigitSame(21, 31)
